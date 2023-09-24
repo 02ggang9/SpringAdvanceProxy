3e5d632cb5c2d94f06fcd6fdda14e9ea55f7c9f1
02ggang9 <02ggang9@gmail.com>
2023-09-24 15:20:20 +0900

    feat(apply): 섹션4 - 인터페이스 기반 프록시와 클래스 기반 프록시

---
+++ src/main/java/hello/proxy/config/v1_proxy/concrete_proxy/OrderServiceConcreteProxy.java
@@ -0,0 +1,36 @@
+package hello.proxy.config.v1_proxy.concrete_proxy;
+
+import hello.proxy.app.v2.OrderRepositoryV2;
+import hello.proxy.app.v2.OrderServiceV2;
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+
+public class OrderServiceConcreteProxy extends OrderServiceV2 {
+
+    private final OrderServiceV2 target;
+    private final LogTrace logTrace;
+
+    public OrderServiceConcreteProxy(OrderServiceV2 target, LogTrace logTrace) {
+        // 문법상 어쩔 수 없이 강제로 호출해야 함.
+        // 프록시를 만들 용도이기 때문에 부모는 안씀.
+        super(null);
+        this.target = target;
+        this.logTrace = logTrace;
+    }
+
+    @Override
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin("OrderService.orderItem()");
+            target.orderItem(itemId);
+            logTrace.end(status);
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
+
+// 단점
+// super(null)
