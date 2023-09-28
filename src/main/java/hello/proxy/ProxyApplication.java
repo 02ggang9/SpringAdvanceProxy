8661ab8eccfd14ec3418cbbb4d30d105938eb33a
02ggang9 <02ggang9@gmail.com>
2023-09-28 13:28:19 +0900

    feat(apply): 빈 후처리기 - 적용

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -5,6 +5,7 @@ import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -18,7 +19,7 @@ import org.springframework.context.annotation.Import;
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
-@Import(ProxyFactoryConfigV1.class)
+@Import(BeanPostProcessorConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
---
+++ src/main/java/hello/proxy/config/v4_postprocessor/postprocessor/PackageLogTracePostProcessor.java
@@ -0,0 +1,42 @@
+package hello.proxy.config.v4_postprocessor.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+@Slf4j
+public class PackageLogTracePostProcessor implements BeanPostProcessor {
+
+    private final String basePackage;
+    private final Advisor advisor;
+
+    public PackageLogTracePostProcessor(String basePackage, Advisor advisor) {
+        this.basePackage = basePackage;
+        this.advisor = advisor;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        //스프링 부트가 만들어주는 수 많은 빈들이 넘어옴
+        log.info("param beanName={} bean={}", beanName, bean.getClass());
+
+        //프록시 적용 대상 여부 체크
+        //프록시 적용 대상이 아니면 원본을 그대로 진행
+        //밑의 코드를 빼면 final 로 등록한 빈들이 있을 경우에 오류가 날 수 있으니 무조건 꼭 필요한 곳에서만 프록시를 적용해야 한다.
+        String packageName = bean.getClass().getPackageName();
+        if (!packageName.startsWith(basePackage)) {
+            return bean;
+        }
+
+        //프록시 대상이면 프록시 만들어서 반환
+        //bean -> target
+        ProxyFactory proxyFactory = new ProxyFactory(bean);
+        proxyFactory.addAdvisor(advisor);
+
+        Object proxy = proxyFactory.getProxy();
+        log.info("create proxy: target={} proxy={}", bean.getClass(), proxy.getClass());
+        return proxy;
+    }
+}
